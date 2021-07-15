c0bbcd77e0369ef7e8955080f490a2b7ff731ee7
zsp <15396226187@163.com>
2021-07-16 01:14:13 +0800

    更新了下第一题的时间复杂度，明天继续耍

--- 题目/src/Solution.java
+++ 题目/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 //给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target的那两个整数，
 // 并返回它们的数组下标。
 //        你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
@@ -22,25 +24,55 @@
 //        进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
 //
 public class Solution {
-    //自己写的
+/**
+ *自己写的，时间复杂度为on^2
+ */
+
+
+//    public  int[] twoSum(int[] nums, int target) {
+//        int []  arr=new int [2];
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int i1 = 1; i1 < nums.length; i1++) {
+//                if (i==i1)break;
+//                if ((nums[i]+nums[i1])==target){
+//                    arr[0]=i1;
+//                    arr[1]=i;
+//                    return arr;
+//                }
+//            }
+//        }
+//        return arr;
+//    }
+
+    /**
+     * 时间复杂度为n
+     * @param nums
+     * @param target
+     * @return
+     */
     public  int[] twoSum(int[] nums, int target) {
         int []  arr=new int [2];
+        if(nums.length==0) return arr;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int i1 = 1; i1 < nums.length; i1++) {
-                if (i==i1)break;
-                if ((nums[i]+nums[i1])==target){
-                    arr[0]=i1;
-                    arr[1]=i;
-                    return arr;
-                }
+            int num =nums[i];
+            int dif =target-num;
+            if (map.containsKey(dif)){
+                arr[0]=map.get(dif);
+                arr[1]=i;
+                return arr;
             }
-        }
+            else{
+                map.put(num,i);
+            }
+            }
+
         return arr;
     }
 
     public static void main(String[] args) {
-        int []  arr={2,5,5,11};
+        int []  arr={2,7,11,159};
         Solution solution = new Solution();
-        solution.twoSum(arr,10);
+        solution.twoSum(arr,9);
     }
 }
