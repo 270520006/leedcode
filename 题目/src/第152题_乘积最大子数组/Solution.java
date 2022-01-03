233559ae592bb753c70a14bc38cf5bdf71ab14f2
zsp <15396226187@163.com>
2022-01-03 20:29:33 +0800

    学习了两道动态规划的题目

---
+++ 题目/src/第152题_乘积最大子数组/Solution.java
@@ -0,0 +1,7 @@
+package 第152题_乘积最大子数组;
+
+public class Solution {
+    public int maxProduct(int[] nums) {
+        return 0;
+    }
+}
--- 题目/src/第53题_最大子序和/Review.java
+++ 题目/src/第53题_最大子序和/Review.java
@@ -2,17 +2,25 @@ package 第53题_最大子序和;
 
 public class Review {
     public int maxSubArray(int[] nums) {
-        int max=nums[0];
+        int max =nums[0];
         int sum=0;
         for (int i = 0; i < nums.length; i++) {
-            if (sum<0){
-                sum=nums[i];
-            }else
-            {
+            if (sum>0){
                 sum+=nums[i];
+            }else{
+                sum=nums[i];
             }
-           max=Math.max(sum,max);
+            max=Math.max(max,sum);
         }
         return max;
     }
+
+
+    public static void main(String[] args) {
+        int []arr ={-2,1,-3,4,-1,2,1,-5,4};
+        Review solution = new Review();
+        int i = solution.maxSubArray(arr);
+        System.out.println(i);
+    }
+
 }
